118e8b7af7c22cca23bbc60edf7b3f2143520cd6
Shawn Leonard <sleonard@microsoft.com>
2019-11-07 23:30:42 -0700

    revising uploader

--- scripts/upload-filetostorage.ps1
+++ scripts/upload-filetostorage.ps1
@@ -1,70 +1,78 @@
-
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory = $true)]
   [ValidateScript( { Test-Path $_ -PathType 'Leaf' })]
   [string]$file,
+  
+  [Parameter(Mandatory = $true)]
+  [string]$storageUri,
+
   [Parameter(Mandatory = $false)]
-  [string]$storageUri = "https://armholder.blob.core.usgovcloudapi.net",
-  [Parameter(Mandatory = $false)]
-  [string]  $sas = "?st=2019-10-25T21%3A03%3A30Z&se=2019-11-26T20%3A03%3A00Z&sp=racwl&sv=2018-03-28&sr=c&sig=b6MXJaJ5JrhiCaXjW3XJ8MedBQjYGUs5hJdpSlI53uQ%3D"
+  [string]$sas = "?st=2019-10-25T21%3A03%3A30Z&se=2019-11-26T20%3A03%3A00Z&sp=racwl&sv=2018-03-28&sr=c&sig=b6MXJaJ5JrhiCaXjW3XJ8MedBQjYGUs5hJdpSlI53uQ%3D"
 )
 BEGIN {
+  $installPath = "C:\\installtools"
   $sasuri = ("{0}/media{1}" -f $storageUri, $sas)
 }
 PROCESS {
 
-  Function Start-Command {
-    Param([Parameter (Mandatory = $true)]
-      [string]$Command, 
-      [Parameter (Mandatory = $true)]
-      [string]$Arguments)
 
-    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-    $pinfo.FileName = $Command
-    $pinfo.RedirectStandardError = $true
-    $pinfo.RedirectStandardOutput = $true
-    $pinfo.CreateNoWindow = $true
-    $pinfo.UseShellExecute = $false
-    $pinfo.Arguments = $Arguments
-    $p = New-Object System.Diagnostics.Process
-    $p.StartInfo = $pinfo
-    $p.Start() | Out-Null
-    $p.WaitForExit()
-    [pscustomobject]@{
-      stdout   = $p.StandardOutput.ReadToEnd()
-      stderr   = $p.StandardError.ReadToEnd()
-      ExitCode = $p.ExitCode  
-    }
-  }
-
-  $azbloburi = ("{0}/files/azcopy_windows_amd64_10.2.1.zip{1}" -f $storageUri, $sas)
+  $azbloburi = "https://aka.ms/downloadazcopy-v10-windows"
   $outputPath = Join-Path -Path "." -ChildPath "scripts" -Resolve
-  $azCopyExe = ("{0}/AzCopy.exe" -f $outputPath)
-
-
-  if ($PSVersionTable.PSEdition -eq "Core") {
-    Install-Module WindowsCompatibility  -Scope CurrentUser 
-    #Import-WinModule BitsTransfer
+  $downloadPath = Join-Path -Path $outputPath -ChildPath "AZCopy"
+  if (-not(Test-Path -Path $downloadPath -PathType Container)) {
+    New-Item -Path $downloadPath -ItemType Directory -Force
   }
+  $azCopyExe = Join-Path -Path $downloadPath -ChildPath "AzCopy.exe"
+
+  if (-not(Test-Path -Path $installPath -PathType Container)) {
+    New-Item -Path $installPath -ItemType Directory -Force
+  }
+
 
   if (-not (Test-Path $azCopyExe)) {
     # download AzCopy from BlobStorage
-    Invoke-WebRequest $azbloburi -OutFile (Join-Path -Path $outputPath -ChildPath "azcopy_windows_amd64_10.2.1.zip")
-    #Start-BitsTransfer -Source $azbloburi -Destination $outputPath
-  
-    Add-Type -assembly "system.io.compression.filesystem"
-    [io.compression.zipfile]::ExtractToDirectory($(Join-Path $outputPath "azcopy_windows_amd64_10.2.1.zip"), $outputPath)
+    $zipFile = (Join-Path -Path $downloadPath -ChildPath "azcopy_windows_amd64.zip")
+    
+    if ($PSVersionTable.PSEdition -eq "Core") {
+      Invoke-WebRequest $azbloburi -OutFile $zipFile
+      #Import-WinModule BitsTransfer
+    }
+    else {
+      Start-BitsTransfer -Source $azbloburi -Destination $zipFile
+    }
 
-    # re-define variable to output path as we downloaded the files just in time
-    $azCopyExe = ("{0}/AzCopy.exe" -f $outputPath)
+    Expand-Archive -Path $zipFile -DestinationPath $installPath -Force -Verbose:$VerbosePreference
+
+    $azcopyInstance = Get-ChildItem -Path $installPath -Filter "azcopy.exe*" -Recurse | sort-object LastWriteTime -Descending | Select-Object -First 1
+    $azCopyExe = $azcopyInstance[0].FullName
+    $azCopyDirectory = $azcopyInstance[0].Directory.Name
+    $azCopyDirectoryFull = $azcopyInstance[0].Directory.FullName
+
+    $pathChanges = $false
+    $userPath = Get-ItemProperty -path "HKCU:\Environment" -Name Path
+    if ($userPath.Path.Contains('azcopy_windows')) {
+      $paths = $userPath.Path -split ';'
+     
+      if (($paths | where-object { $_.Contains($azCopyDirectory) } | Measure-Object).Count -le 0) {
+        # most likely the previous version is in the path
+        $amd = $paths | where-object { $_.Contains('azcopy_windows') } 
+        $newPath = $userPath.Path.Replace(("{0};" -f $amd), ("{0};" -f $azCopyDirectoryFull))
+        $pathChanges = $true
+      }
+    }
+    else {
+      $newPath = ("{0};{1}" -f $userPath.Path, $azCopyDirectoryFull)
+      $pathChanges = $true
+    }
+    
+    if ($pathChanges -eq $true) {
+      Set-ItemProperty -Path "HKCU:\Environment" -Name Path -Value $newPath
+    }
   }
 
 
 
-  $result = Start-Command -Command "`"$azCopyExe`"" -Arguments "$file" "$sasuri"
   Write-Host $result
-  
-
-
+  azcopy copy $file $sasuri
 }
