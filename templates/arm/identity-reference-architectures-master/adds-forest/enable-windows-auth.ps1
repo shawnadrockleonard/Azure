ba5e5e4384b03d7a6f2e320dbb821d29e32c7273
Shawn Leonard <sleonard@microsoft.com>
2020-01-28 18:01:16 -0700

    more cleanup

--- templates/arm/identity-reference-architectures-master/adds-forest/enable-windows-auth.ps1
+++
@@ -1,5 +0,0 @@
-﻿Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/windowsAuthentication" -Name Enabled -Value True -PSPath "IIS:\Sites\$SiteName\$AppName"
-
-Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/anonymousAuthentication" -Name Enabled -Value False -PSPath "IIS:\Sites\$SiteName\$AppName"
-
-IISRESET
