eb0f5307ae5a9cb80cff44547581b8572bc42c06
sleonard <sleonard@microsoft.com>
2020-10-30 16:02:10 -0500

    removing nested portions

--- AzureDevTestLabs/Environments/WebApp-Identity-KeyVault/test-deployer.ps1
+++ AzureDevTestLabs/Environments/WebApp-Identity-KeyVault/test-deployer.ps1
@@ -1,18 +1,17 @@
 $uri = (Get-ChildItem Env:\dep_uri).Value
 $sas = (Get-ChildItem Env:\dep_sas).Value
-$sassecret = ConvertTo-SecureString -String $sas -AsPlainText
-$sqlpassword = ConvertTo-SecureString -String (Get-ChildItem Env:\dep_password).Value -AsPlainText
+$sqlpassword = ConvertTo-SecureString -String (Get-ChildItem Env:\dep_password).Value -AsPlainText -Force
 
 
 New-AzResourceGroupDeployment -Name "testdeploy" -ResourceGroupName "devtest-labs-va-DevSecure-024011" -Mode Incremental `
     -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json `
-    -_artifactsLocation $uri -_artifactsLocationSasToken $sassecret -DatabaseAdministratorLoginPassword $sqlpassword `
+    -DatabaseAdministratorLoginPassword $sqlpassword `
     -Verbose -WhatIf
 
 
 New-AzResourceGroupDeployment -Name "testdeploy" -ResourceGroupName "devtest-labs-va-DevSecure-024011" -Mode Incremental `
     -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json `
-    -_artifactsLocation $uri -_artifactsLocationSasToken $sassecret -DatabaseAdministratorLoginPassword $sqlpassword `
+    -DatabaseAdministratorLoginPassword $sqlpassword `
     -Verbose
 
     
